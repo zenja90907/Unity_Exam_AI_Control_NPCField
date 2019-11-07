cfaa5d4b07557306efde55427ca23b57d1b25b91
unknown <zenja90907@gmail.com>
2019-11-07 11:55:26 +0800

    期中考完成

---
+++ UnityProject/Assets/Ball.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [Header("速度")]
+    [Range(0, 100)]
+    public int speed = 50;
+    [Header("跳躍")]
+    [Range(0, 300)]
+    public int jump = 0;
+    [Header("是否在地板上")]
+    public bool IsGround=false;
+
+    private Rigidbody r3d;
+
+    private void Start()
+    {
+        r3d = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Walk();
+        Jump();
+    }
+
+    private void Walk()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+            r3d.AddForce(new Vector3(0, 0, speed));
+        if (Input.GetKey(KeyCode.S))
+            r3d.AddForce(new Vector3(0, 0, -speed));
+        if (Input.GetKey(KeyCode.A))
+            r3d.AddForce(new Vector3(-speed, 0,0));
+        if (Input.GetKey(KeyCode.D))
+            r3d.AddForce(new Vector3(speed, 0,0));
+    }
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            r3d.AddForce(new Vector3(0, jump, 0));
+    }
+}
---
+++ UnityProject/Assets/Robot.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Robot : MonoBehaviour
+{
+    [Header("對話")]
+    public string say = "你好.....";
+    public float sayspeed= 1.5f;
+    [Header("任務")]
+    public bool Compiete=false;
+    public int bateryfinish = 5;
+    public int batery = 0;
+}
